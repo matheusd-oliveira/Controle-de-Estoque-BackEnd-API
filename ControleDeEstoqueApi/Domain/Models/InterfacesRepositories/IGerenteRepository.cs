8813fd91a5c42133e627cd9b189187d8917f2ef2
Matheus Oliveira <matheussilvaolveira@gmail.com>
2024-06-17 00:00:02 -0300

    Refatorando Interface IGerenteRepository. Lembrar de retornar a essa interface para definir o método de RelatorioDeVendasNoMes

--- ControleDeEstoqueApi/Domain/Models/InterfacesRepositories/IGerenteRepository.cs
+++ ControleDeEstoqueApi/Domain/Models/InterfacesRepositories/IGerenteRepository.cs
@@ -1,27 +1,30 @@
-﻿namespace ControleDeEstoqueApi.Domain.Models.InterfacesRepositories
+﻿using ControleDeEstoqueApi.Domain.Models.Agents;
+
+namespace ControleDeEstoqueApi.Domain.Models.InterfacesRepositories
 {
     public interface IGerenteRepository : IVendedorRepository
-    {   
+    {
         // TODO: Adicionar async aos métodos
 
         // Gerar Relatorio
-        public void ExibirRelatorioDeVendasDoMes() { }
-        public void ExibirRelatorioDeVendasNoDiaVigente() { }
+        // TODO: PENSAR EM COMO EXIBIR ESSAS VENDAS.
+        public Task ExibirRelatorioDeVendasDoMes();
+        public Task<IEnumerable<Venda>> ExibirRelatorioDeVendasNoDiaVigente(DateTime diaVigente);
 
         // Manter Funcionario
-        public void CadastrarFuncionario() { }
-        public void AlterarFuncionario() { }
-        public void ListarFuncionarios() { }
+        public Task CadastrarFuncionario(Funcionario funcionario);
+        public Task AlterarFuncionario(int codigoDoFuncionario);
+        public Task<IEnumerable<Funcionario>> ListarFuncionarios();
 
         // Manter Fornecedor
-        public void CadastrarFornecedor() { }
-        public void AlterarFornecedor() { }
-        public void ListarFornecedores() { }
+        public Task CadastrarFornecedor(Fornecedor fornecedor);
+        public Task AlterarFornecedor(int codigoDoFornecedor);
+        public Task<IEnumerable<Fornecedor>> ListarFornecedores();
 
         // Manter Fabricante
-        public void CadastrarFabricante() { }
-        public void AlterarFabricante() { }
-        public void ListarFabricantes() { }
+        public Task CadastrarFabricante(Fabricante fabricante);
+        public Task AlterarFabricante(int codigoDoFabricante);
+        public Task<IEnumerable<Fabricante>> ListarFabricantes();
 
     }
 }

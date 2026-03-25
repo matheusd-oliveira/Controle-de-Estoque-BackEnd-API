69d0223f5be5f5acb7ab7632e4fff224c6305f0f
Matheus Oliveira <matheussilvaolveira@gmail.com>
2026-03-24 22:11:47 -0300

    Refactor estoque

--- Domain/Models/Estoque.cs
+++ Domain/Models/Estoque.cs
@@ -5,46 +5,22 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ControleDeEstoqueApi.Domain.Models
 {
-    [Table("estoque")]
     public class Estoque
     {
-        public Estoque(
-            int codigo_do_funcionario,
-            int codigo_do_produto,
-            string nome_do_produto,
-            string nome_fantasia_do_fornecedor,
-            string nome_do_fabricante,
-            int quantidade_do_produto)
+
+        public Estoque(int produtoId, int quantidade)
         {
-            this.codigo_do_funcionario = codigo_do_funcionario;
-            this.codigo_do_produto = codigo_do_produto;
-            this.nome_do_produto = nome_do_produto;
-            this.nome_fantasia_do_fornecedor = nome_fantasia_do_fornecedor;
-            this.nome_do_fabricante = nome_do_fabricante;
-            this.quantidade_do_produto = quantidade_do_produto;
+            ProdutoId = produtoId;
+            Quantidade = quantidade;
+
+            if (ProdutoId <= 0)
+                throw new Exception("Produto obrigatório");
+            if (Quantidade <= 0)
+                throw new Exception("Quantidade inválida");
+
         }
-
-        [Key]
-        public int id_estoque { get; set; }
-
-        [ForeignKey("Funcionario")]
-        public int codigo_do_funcionario { get; set; }
-
-        [ForeignKey("Produto")]
-        public int codigo_do_produto { get; set; }
-
-        [MaxLength(250)] // Adicionado o nome para ser identificado no estoque
-        public string nome_do_produto { get; set; } // TESTE
-
-        [ForeignKey("Fornecedor")]
-        public string nome_fantasia_do_fornecedor { get; set; }
-
-        [ForeignKey("Fabricante")]
-        public string nome_do_fabricante { get; set; }
-        public int quantidade_do_produto { get; set; }
-        public Produto Produto { get; set; } // Criando propriedade de navegação para acessar a tabela Produto
-        public Fabricante Fabricante { get; set; } // Criando propriedade de navegação para acessar a tabela Fabricante
-        public Fornecedor Fornecedor { get; set; } // Criando propriedade de navegação para acessar a tabela Fornecedor
-        public Funcionario Funcionario { get; set; } // Criando propriedade de naveção para acessar a tabela Funcionário
+        public int Id { get; set; }
+        public int ProdutoId { get; set; }
+        public int Quantidade { get; set; }
     }
 }
